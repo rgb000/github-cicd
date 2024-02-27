d667d4da8c0f3215b43234ec488f112a7a9bdd98
Oleksandr Gubernatorov <virmagnavi2010@gmail.com>
2024-02-27 19:42:22 +0200

    feat: add api module

---
+++ modules/api/output.tf
@@ -0,0 +1,9 @@
+output "name" {
+  value       = azurerm_container_registry.this.name
+  description = "Resource name"
+}
+
+output "id" {
+  value       = azurerm_container_registry.this.id
+  description = "Resource ID"
+}
